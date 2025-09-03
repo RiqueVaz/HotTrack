0ea2f274b2961fd686280297c4ad7379cf2e4132
azag666 <157975443+azag666@users.noreply.github.com>
2025-09-03 02:38:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('push', function(event) {
     const data = event.data.json();
     const options = {
         body: data.body,
-        icon: 'https://i.postimg.cc/VL8bKCD9/Gemini-Generated-Image-lejmdflejmdflejm-removebg-preview.png', // Ícone moderno
+        icon: 'icons/icon-192x192.png', // Caminho atualizado
     };
     event.waitUntil(
         self.registration.showNotification(data.title, options)
