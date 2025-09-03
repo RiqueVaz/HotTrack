08668b49c63226b41f8c399b44dd099e060efeaf
azag666 <157975443+azag666@users.noreply.github.com>
2025-09-03 02:54:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('push', function(event) {
     const data = event.data.json();
     const options = {
         body: data.body,
-        icon: 'https://ibb.co/wFvCyfyT', // Caminho atualizado
+        icon: 'https://i.postimg.cc/ZnHB6HB5/icon-512x512.png', // Novo link aqui
     };
     event.waitUntil(
         self.registration.showNotification(data.title, options)
